ca2d3bbc69275986170cae604e118da7bc8efa25
Ursu Nicolae <ursu.nicolae@isa.utm.md>
2022-10-24 14:54:22 +0300

    Add initiation of subscriber and pipeline in main

--- PTRLab/src/main/scala/Main.scala
+++ PTRLab/src/main/scala/Main.scala
@@ -1,5 +1,16 @@
+import Subscription.{PipeLine, Subscriber}
+import Topics.CreateListener
+import akka.actor.{ActorSystem, Props}
+
 object Main {
   def main(args: Array[String]): Unit ={
     Console.printf("Starting message broker");
+    val system = ActorSystem()
+    val subscriber = system.actorOf(Props[Subscriber], "subscriber")
+    val pipeline = system.actorOf(Props[PipeLine], "pipeline")
+    var port = 9000
+    pipeline ! CreateListener(port)
+    port += 1
+    pipeline ! CreateListener(port)
   }
 }
